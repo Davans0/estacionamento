3d526ccbaf8e47b8a1bc812797a5b09732535c03
Davans0 <davanso420@gmail.com>
2022-07-14 17:08:14 -0300

    primeiro commit

---
+++ Reactjs/src/index.js
@@ -0,0 +1,19 @@
+import React from "React"
+import ReactDOM from "React-dom"
+import "./stles.css"
+
+
+
+
+const app = () => {
+
+    return {
+      <div className="app">
+         hello world
+      </div>
+    }
+}
+
+
+const rootelement = document.getelementbyid("root")
+ReactDOM.render(<app />, rootelement) 
